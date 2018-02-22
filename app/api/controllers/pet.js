25351e51dbc6c8eef7023d2fe1fc7b8561c50c4a
Peter Henell <mail@peterhenell.se>
2018-02-22 08:32:25 +0000

    working get with parameter name

--- app/api/controllers/pet.js
+++ app/api/controllers/pet.js
@@ -1,10 +1,22 @@
 'use strict';
 
 
-module.exports = {
-    getPetByName: function(req, res) {
-	      res.json({
-	          name: req.swagger.params.name
-	      });
-    }
+module.exports = {getPetByName, put, post}
+
+
+function getPetByName(req, res, next) {
+    console.log('getPetByName');
+    res.json({
+        name: req.swagger.params.name.value,
+        photoUrls: [],
+        id: 1,
+    });
 }
+
+function post(req, res) {
+    console.log('post');
+}
+function put(req, res) {
+    console.log('put');
+}
+
